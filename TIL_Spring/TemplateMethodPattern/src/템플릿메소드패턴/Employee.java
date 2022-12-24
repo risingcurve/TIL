ba5705209c5e08978d4bd8526084fc30d54f74f6
risingcurve <gksdnjswhd10@naver.com>
2022-12-24 16:48:58 +0900

    gg

---
+++ TIL_Spring/TemplateMethodPattern/src/템플릿메소드패턴/Employee.java
@@ -0,0 +1,11 @@
+package 템플릿메소드패턴;
+
+public class Employee {
+	private String name;
+	public void doSomething() {
+		System.out.println("출근한다.");
+		System.out.println("소와 같이 일을 한다.");
+		System.out.println("웰스토리를 먹는다.");
+		System.out.println("퇴실체크한다.");
+	}
+}
---
+++ TIL_Spring/TemplateMethodPattern/src/템플릿메소드패턴/Student.java
@@ -0,0 +1,11 @@
+package 템플릿메소드패턴;
+
+public class Student {
+	private String name;
+	public void doSomething() {
+		System.out.println("입실체크한다.");
+		System.out.println("공부한다.");
+		System.out.println("급식을 먹는다.");
+		System.out.println("퇴실체크한다.");
+	}
+}
