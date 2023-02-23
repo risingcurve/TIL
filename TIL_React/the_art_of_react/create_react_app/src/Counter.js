d4aebdeaf6d63d3e0f48d429a8bf0d7a7343da6e
risingcurve <gksdnjswhd10@naver.com>
2023-02-23 13:08:03 +0900

    the art of react

---
+++ TIL_React/the_art_of_react/create_react_app/src/Counter.js
@@ -0,0 +1,31 @@
+import { Component } from "react";
+
+class Counter extends Component {
+    constructor(props) {
+        super(props)
+        // state의 초기값 설정
+        this.state = {
+            number: 0
+        }
+    }
+
+    render() {
+        const { number } = this.state; // state를 조회할 때는 this.state로 조회합니다.
+        return (
+            <div>
+                <h1>{number}</h1>
+                <button
+                    // onClick을 통해 버튼이 클릭되었을 때 호출할 함수를 지정
+                    onClick={() => {
+                        // this.setState를 사용하여 state에 새로운 값을 넣을 수 있습니다.
+                        this.setState({ number: number + 1 })
+                    }}
+                >
+                    +1
+                </button>
+            </div>
+        )
+    }
+}
+
+export default Counter
