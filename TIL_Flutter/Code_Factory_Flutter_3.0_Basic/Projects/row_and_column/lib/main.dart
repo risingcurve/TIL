48921119d64ecc0472f8564019860a98f3324a2a
gksdnjswhd10 <gksdnjswhd10@naver.com>
2023-06-16 04:05:21 +0900

    flutter : row and column

---
+++ TIL_Flutter/Code_Factory_Flutter_3.0_Basic/Projects/row_and_column/lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:row_and_column/screen/home_screen.dart';
+
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: HomeScreen(),
+    ),
+  );
+}
+
+
+
+
---
+++ TIL_Flutter/Code_Factory_Flutter_3.0_Basic/Projects/row_and_column/lib/screen/home_screen.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        bottom: false,
+        child: Container(
+          color: Colors.black,
+          child: Row(
+            // MainAxisAlignment - 주축 정렬
+            // start - 시작
+            // end - 끝
+            // center - 가운데
+            // spaceBetween - 위젯과 위젯의 사이가 동일하게 배치된다.
+            // spaceEvenly - 위젯을 같은 간격으로 배치하지만 끝과 끝에도 위젯이 아닌 빈 간격으로 시작한다.
+            // spaceAround - spaceEvenly + 끝과 끝의 간격은 1/2
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Container(
+                color: Colors.red,
+                width: 50.0,
+                height: 50.0,
+              ),
+              Container(
+                color: Colors.orange,
+                width: 50.0,
+                height: 50.0,
+              ),
+              Container(
+                color: Colors.yellow,
+                width: 50.0,
+                height: 50.0,
+              ),
+              Container(
+                color: Colors.green,
+                width: 50.0,
+                height: 50.0,
+              ),
+            ],
+          )
+        ),
+      )
+    );
+  }
+}
