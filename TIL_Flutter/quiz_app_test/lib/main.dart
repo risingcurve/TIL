21276163ffd88f3a073a181ac11b95ea846f9e34
gksdnjswhd10 <gksdnjswhd10@naver.com>
2023-07-23 23:46:32 +0900

    flutter : quiz app test

---
+++ TIL_Flutter/quiz_app_test/lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:quiz_app_test/screen/screen_home.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'My Quiz App',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
