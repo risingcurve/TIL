1d2c362036fb497cb1e670917b836356cbd07522
gksdnjswhd10 <gksdnjswhd10@naver.com>
2023-08-04 23:48:48 +0900

    js

--- TIL_Javascript/nomadcoder_momentum/js/todo.js
+++ TIL_Javascript/nomadcoder_momentum/js/todo.js
@@ -2,15 +2,26 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = document.querySelector("#todo-form input");
 const toDoList = document.getElementById("todo-list");
 
+const TODOS_KEY = "toDos";
+
+let toDos = [];
+
+function saveToDos () {
+  localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
+}
+
 function deleteToDo(e) {
   const li = e.target.parentElement;
   li.remove();
+  toDos = toDos.filter((toDo) => toDo.id != parseInt(li.id));
+  saveToDos();
 }
 
 function paintTodo(newTodo) {
   const li = document.createElement("li");
+  li.id = newTodo.id;
   const span = document.createElement("span");
-  span.innerText = newTodo;
+  span.innerText = newTodo.text;
   const button = document.createElement("button");
   button.innerText = "❌";
   button.addEventListener("click", deleteToDo);
@@ -23,7 +34,26 @@ function handleTodoSubmit(e) {
   e.preventDefault();
   const newTodo = toDoInput.value;
   toDoInput.value = "";
-  paintTodo(newTodo);
+  const newTodoObj = {
+    text:newTodo,
+    id: Date.now(),
+  }
+  toDos.push(newTodoObj);
+  paintTodo(newTodoObj);
+  saveToDos();
 }
 
 toDoForm.addEventListener("submit", handleTodoSubmit);
+
+// function sayHello(item) {
+//   console.log("this is the turn of", item);
+// }
+
+
+const savedToDos = localStorage.getItem(TODOS_KEY);
+
+if(savedToDos !== null) {
+  const parsedToDos = JSON.parse(savedToDos);
+  toDos = parsedToDos;
+  parsedToDos.forEach(paintTodo);
+}
