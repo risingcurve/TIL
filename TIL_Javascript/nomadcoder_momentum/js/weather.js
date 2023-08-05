cc6c15915659a2378bc44b6d76c673f4f06db1cf
gksdnjswhd10 <gksdnjswhd10@naver.com>
2023-08-05 16:32:42 +0900

    js

--- TIL_Javascript/nomadcoder_momentum/js/weather.js
+++ TIL_Javascript/nomadcoder_momentum/js/weather.js
@@ -1,7 +1,16 @@
 function onGeoOk(position) {
   const lat = position.coords.latitude;
-  const lng = position.coords.longitude;
-  console.log(lat, lng);
+  const lon = position.coords.longitude;
+  const WEATHER_API = config.API_KEY
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${WEATHER_API}&units=metric`;
+  fetch(url)
+    .then(res => res.json())
+    .then(data => {
+      const weather = document.querySelector('#weather span:first-child');
+      const city = document.querySelector('#weather span:last-child');
+      weather.innerText = data.name;
+      city.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+  });
 }
 
 function onGeoError() {
@@ -9,4 +18,4 @@ function onGeoError() {
 }
 
 
-navigator.geolocation.getCurrentPosition();
+navigator.geolocation.getCurrentPosition(onGeoOk,onGeoError);
